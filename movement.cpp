c3ce510f3ac7d0a56c7036dbcede3b3386fe478c
benjaminvantonder <vantonderbenna@gmail.com>
2026-01-23 16:13:41 +0200

    Movement bounds for orc object

--- movement.cpp
+++ movement.cpp
@@ -1,13 +1,18 @@
 #include "main.h"
 #include "movement.h"
 #include "orcTextures.h"
+#include "towerTextures.h"
 
 // This file is for handling npc movement
 // This tower defense won't have manual player input for movement
 
 void moveOrc() {
 
-    orc.position.x = orc.position.x + 1;
+    if(orc.position.x >= (towerRect.x) + towerRect.width) {
+
+        orc.position.x = orc.position.x - 5;
+
+    }
 
 }
 
