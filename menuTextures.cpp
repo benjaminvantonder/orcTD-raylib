74d2df8147fd6588649587700ba162c7f94e4e07
benjaminvantonder <vantonderbenna@gmail.com>
2026-01-24 14:34:15 +0200

    menu rendering non operational

---
+++ menuTextures.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+#include "main.h"
+#include "menuTextures.h"
+
+Texture2D menu2D;
+
+void loadMainMenu() {
+    menu2D = LoadTexture("./assets/Orc logo.png");
+}
+
+void drawButton(Rectangle Button, char Text[]) {
+    DrawRectangleRec(Button, LIME);
+    DrawRectangleLinesEx(Button, 2, LIME);
+    DrawText(Text, Button.x + 10, Button.y + 10, 20, BLACK);
+}
+
+void drawMainMenu() {
+    // Menu PNG
+    Rectangle source = {0, 0, PNG_WIDTH, PNG_HEIGHT};
+    Rectangle dest = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, source.width, source.height};
+    DrawTexturePro(menu2D, source, dest, {dest.width / 2, 500}, 0, WHITE);
+
+    // Buttons
+}
---
+++ menuTextures.h
@@ -0,0 +1,5 @@
+#define PNG_WIDTH 900
+#define PNG_HEIGHT 500
+
+void loadMainMenu();
+void drawMainMenu();
